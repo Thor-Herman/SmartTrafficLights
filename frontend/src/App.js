367b3574986f25204b7178dcd943982289c3c849
Thor-Herman van Eggelen <44784559+Thor-Herman@users.noreply.github.com>
2022-03-29 10:37:04 +0100

    Change app state to handle several lights

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import ChartComponent from './ChartComponent';
+import { SERVER_URL } from './consts';
 
 const App = () => {
   const [state, setState] = useState({
-    currentX: Math.round(Math.random()),
-    time: 0,
+    1: { currentX: 0, time: 0 },
+    2: { currentX: 0, time: 0 },
   });
 
   useEffect(
     () =>
       setInterval(async () => {
-        await fetch('http://localhost:8081/readTrafficLightState?tf_id=1')
+        await fetch(SERVER_URL + '/readTrafficLightState?tf_id=1')
           .then((response) => response.text())
           .then((data) => {
             setState((state) => {
-              const newTime = Number(state.time) + 0.5;
-              const newCurrentX = data;
-              return { currentX: newCurrentX, time: newTime.toFixed(1) };
+              const newTime = Number(state[1].time) + 0.5;
+              const newCurrentX = 1;
+              return {
+                ...state,
+                1: { currentX: newCurrentX, time: newTime.toFixed(1) },
+              };
             });
             console.log(data);
           });
@@ -27,7 +31,7 @@ const App = () => {
 
   return (
     <div className='app'>
-      <ChartComponent currentX={state.currentX} time={state.time} />
+      <ChartComponent currentX={state[1].currentX} time={state[1].time} />
     </div>
   );
 };
