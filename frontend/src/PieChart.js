6e831bfb7f91204cbc807b839f3b57970eabbb1d
Thor-Herman van Eggelen <44784559+Thor-Herman@users.noreply.github.com>
2022-04-05 10:21:32 +0100

    Begin basic pie chart

---
+++ frontend/src/PieChart.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { Pie } from 'react-chartjs-2';
+import { pieChartOptions } from './chartConfig';
+import { SERVER_URL } from './consts';
+import { customCanvasBackgroundColor } from './plugins';
+
+const PieChart = () => {
+  const [trafficLightId, setTrafficLightId] = useState(0);
+  const [trafficLightPercentages, setTrafficLightPercentages] = useState({
+    GREEN: 33,
+    RED: 33,
+    YELLOW: 33,
+  });
+
+  useEffect(() => {
+    const response = fetch(
+      SERVER_URL + '/gridTrafficLightMetrics?tf_id=' + trafficLightId
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setTrafficLightPercentages({
+          GREEN: data.greenDuration,
+          RED: data.redDuration,
+          YELLOW: data.yellowDuration,
+        });
+      });
+  }, [trafficLightId]);
+
+  const data = {
+    labels: ['Red', 'Green', 'Yellow'],
+    datasets: [
+      {
+        label: 'Percentage of light status',
+        data: [
+          trafficLightPercentages.RED,
+          trafficLightPercentages.GREEN,
+          trafficLightPercentages.YELLOW,
+        ],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 140, 120)',
+          'rgb(255, 205, 86)',
+        ],
+      },
+    ],
+  };
+
+  return (
+    <div className='pie-chart'>
+      <div>
+        <Pie
+          options={pieChartOptions}
+          data={data}
+          plugins={[customCanvasBackgroundColor]}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PieChart;
